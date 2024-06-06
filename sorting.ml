fe2962182e55e1c5c9cc975c52ed773d15afc219
ishandahal <ishan.dahal88@gmail.com>
2024-06-06 12:05:33 -0400

    Finish exercises for sorting

--- sorting.ml
+++ sorting.ml
@@ -42,3 +42,42 @@ let rec merge_sort lst =
 let _ = assert (merge_sort [] = [])
 let _ = assert (merge_sort [ 2; 1; 3 ] = [ 1; 2; 3 ])
 let _ = assert (merge_sort [ 9; 8; 7; 6; 5; 4 ] = [ 4; 5; 6; 7; 8; 9 ])
+
+(** [insert_rev elem lst] is elem placed in appropriate location in reverse sorted lst*)
+let rec insert_rev elem = function
+  | [] -> [ elem ]
+  | h :: t -> if elem > h then elem :: h :: t else h :: insert_rev elem t
+
+(** [insertion_sort_rev lst] is lst sorted in reverse order *)
+let rec insertion_sort_rev = function
+  | [] -> []
+  | h :: t -> insert_rev h (insertion_sort_rev t)
+
+let _ = assert (insertion_sort_rev [] = [])
+let _ = assert (insertion_sort_rev [ 1; 2; 3 ] = [ 3; 2; 1 ])
+let _ = assert (insertion_sort_rev [ 2; 1; 3 ] = [ 3; 2; 1 ])
+
+(** [is_sorted lst] is true if lst is sorted false otherwise. Returns true if lst is empty.*)
+let rec is_sorted = function
+  | [] -> true
+  | [ _ ] -> true
+  | f :: s :: t -> f <= s && is_sorted t
+
+let _ = assert (is_sorted [ 1; 2; 3; 4 ] = true)
+let _ = assert (not (is_sorted [ 2; 3; 4; 1 ] = true))
+
+(** [sort lst] is sorted lst. Implementation where both functions are combined in one.*)
+let sort =
+  let rec insert_rev elem = function
+    | [] -> [ elem ]
+    | h :: t -> if elem < h then elem :: h :: t else h :: insert_rev elem t
+  in
+  let rec sort_helper = function
+    | [] -> []
+    | h :: t -> insert_rev h (sort_helper t)
+  in
+  sort_helper
+
+let _ = assert (sort [] = [])
+let _ = assert (sort [ 2; 1; 3 ] = [ 1; 2; 3 ])
+let _ = assert (sort [ 9; 8; 7; 6; 5; 4 ] = [ 4; 5; 6; 7; 8; 9 ])
